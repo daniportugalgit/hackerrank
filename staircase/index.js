32982238c8d4a0342d9f8617d18dd7bf9d67614f
Daniel Portugal <daniportugal@gmail.com>
2020-09-09 17:10:46 -0300

    best way

--- staircase/index.js
+++ staircase/index.js
@@ -1,19 +1,6 @@
 function staircase(n) {
   for (let i = 1; i <= n; i++) {
-    let spaces = n - i;
-    let spaceString = '';
-    let hashtags = i;
-    let hashtagString = '';
-
-    for (let j = 0; j < spaces; j++) {
-      spaceString += ' ';
-    }
-
-    for (let k = 0; k < hashtags; k++) {
-      hashtagString += '#';
-    }
-
-    console.log(spaceString + hashtagString);
+    console.log('#'.repeat(i).padStart(n));
   }
 }
 
