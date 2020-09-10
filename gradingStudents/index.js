4a7cb5c0e2e9f1ee32487d0e1184e5fa6243571e
Daniel Portugal <daniportugal@gmail.com>
2020-09-10 10:06:59 -0300

    using forEach and not changing the original array

--- gradingStudents/index.js
+++ gradingStudents/index.js
@@ -1,12 +1,14 @@
 function gradingStudents(grades) {
-  for (let i = 0; i < grades.length; i++) {
-    if(grades[i] < 38 || grades[i] % 5 < 3)
-      continue;
+  let roundedGrades = [];
 
-    grades[i] = Math.ceil(grades[i] / 5) * 5;
-  }
+  grades.forEach(grade => {
+    if(grade < 38 || grade % 5 < 3)
+      roundedGrades.push(grade);
+    else
+      roundedGrades.push(Math.ceil(grade / 5) * 5);
+  });
 
-  return grades;
+  return roundedGrades;
 }
 
 let roundedGrades = gradingStudents([73, 67, 38, 33]); //should return [75, 67, 40, 33]
