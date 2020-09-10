0f0b99a444251663e8b9b5367d84a5eca8eabaaa
Daniel Portugal <daniportugal@gmail.com>
2020-09-10 10:10:34 -0300

    even smaller, but less readable, in comment

--- gradingStudents/index.js
+++ gradingStudents/index.js
@@ -9,6 +9,9 @@ function gradingStudents(grades) {
   });
 
   return roundedGrades;
+
+  //Or, even 'smaller', but much less readable:
+  //return grades.map(grade => (grade < 38 || grade % 5 < 3) ? grade : Math.ceil(grade / 5) * 5);
 }
 
 let roundedGrades = gradingStudents([73, 67, 38, 33]); //should return [75, 67, 40, 33]
